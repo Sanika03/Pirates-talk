a3c0d3241326c23cc0c849814e0b36a299b41d46
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-09-04 20:18:32 +0530

    added processing

--- app.js
+++ app.js
@@ -5,5 +5,16 @@ var outputDiv = document.querySelector("output-div");
 var serverUrl = "https://api.funtranslations.com/translate/pirate.json";
 
 function getUrl(input){
-    return serverurl + "?" + "text=" + input;
+    return (serverUrl + "?" + "text=" + input);
+}
+
+function clickHandler(){
+    var inputText = textInput.value;
+    
+    fetch(getUrl(inputText))
+        .then(response => response.json)
+        .then(json => {
+            var outputText = json.contents.translation;
+            outputDiv.innerText = outputText;
+        })
 }
