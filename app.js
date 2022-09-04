fb93c5e6874f45545a15993fd3449e1ff9ec0f93
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-09-04 20:06:07 +0530

    url construction done

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ var textInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("output-div");
 
+var serverUrl = "https://api.funtranslations.com/translate/pirate.json";
+
+function getUrl(input){
+    return serverurl + "?" + "text=" + input;
+}
