aed93ecec11e5b2adc7e50464cd16f86f66515e6
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-09-06 22:04:26 +0530

    fix : fixed bug

--- app.js
+++ app.js
@@ -4,25 +4,24 @@ var outputDiv = document.querySelector("#output-div");
 
 var serverUrl = "https://api.funtranslations.com/translate/pirate.json";
 
-function errorHandling(error){
+function errorHandling(error) {
     console.log("Error occurred : " + error);
     alert("Server error occurred. Please try again later");
 }
 
-function getUrl(input){
-    return (serverUrl + "?" + "text=" + input);
+function getUrl(input) {
+    return (serverUrl + "?text=" + input);
 }
 
-function clickHandler(){
+function clickHandler() {
     var inputText = textInput.value;
-
     fetch(getUrl(inputText))
         .then(response => response.json())
         .then(json => {
             var outputText = json.contents.translated;
-            outputDiv.innerHTML = outputText;
+            console.log(outputText)
+            outputDiv.innerText = outputText;
         })
         .catch(errorHandling);
 }
-
-btnTranslate.addEventListener("Clicked", clickHandler);
+btnTranslate.addEventListener("click", clickHandler);
